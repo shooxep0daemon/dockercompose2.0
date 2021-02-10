dfec99dce0ecf2fb20236086e52543586e1758d0
shooxep-demon <76490920+shooxep-demon@users.noreply.github.com>
2021-02-10 19:58:51 +0300

    v4 mstp1

--- app/src/main/java/helloworld.java
+++ app/src/main/java/helloworld.java
@@ -1,7 +1,7 @@
 package com.app;
 
-public class HelloWorld {
-    public static void main(String[] argv) {
-        System.out.println("Hello world");
+public class helloworld  {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
